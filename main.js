1acd23c42942973a35e2dea3b37101ce855c9296
accountunblock <youngzecco@gmail.com>
2024-10-29 15:31:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//let userName = "";
+//let defaultName;
+
+/*if (userName){
+  defaultName = userName
+} else {
+  defaultName = "Stranger";
+}*/
+//let defaultName = userName|| "Stranger";
+
+/*console.log(defaultName);
+
+let greeting = 6;
+greeting  ? console.log("Good morning") : console.log("Good afternoon");*/
+
+
+
+/*let isLocked = false;
+isLocked ? console.log("You will need a key to open the door.") : console.log("You will not need a key to open the door");*/
+
+//array
+/*let person = {
+  firstName: "Ezekiel",
+  lastName: "Obang"
+};
+
+
+//Object 
+let fruits = ["Banana", "Oranges", "Apples"];*/
